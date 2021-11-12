466815f6ee5dab18a752ce9cc3c443947c47781a
Pirmsho <d.pirmisashvili@gmail.com>
2021-11-12 17:37:43 +0100

    + Project detail page

--- components/UI/ProjectDetail.tsx
+++ components/UI/ProjectDetail.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+import Image from "next/dist/client/image";
 import styles from "./ProjectDetail.module.scss";
+
+import { motion } from "framer-motion";
 
 const ProjectDetail = ({ project }: any) => {
   return (
-    <section className={styles.detail_section}>
+    <motion.section
+      initial={{ scale: 0.5, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      className={styles.detail_section}
+    >
       <div className={styles.detail_header}>
         <h1>{project.name}</h1>
         <a href={project.github_link} rel="noreferrer" target="_blank">
@@ -14,7 +20,23 @@ const ProjectDetail = ({ project }: any) => {
           See The App In Action
         </a>
       </div>
-    </section>
+      <div className={styles.detail_body}>
+        <div className={styles.img_div}>
+          <Image
+            src={project.image}
+            layout="fill"
+            objectFit="contain"
+            alt="project"
+            quality={100}
+          />
+        </div>
+        <div className={styles.text_div}>
+          <h2>Built With: {project.built_with}</h2>
+          <h3>{project.description_sm}</h3>
+        </div>
+      </div>
+      <p>{project.description_lg}</p>
+    </motion.section>
   );
 };
 
