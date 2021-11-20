ab15b034041425c4f7ca459303efda22c7450b1b
Pirmsho <d.pirmisashvili@gmail.com>
2021-11-20 20:35:05 +0100

    + req

--- components/UI/Footer.tsx
+++ components/UI/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { SyntheticEvent, useState } from "react";
 
 import styles from "./Footer.module.scss";
 
@@ -7,18 +7,71 @@ const Footer = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    console.log("sending");
+
+    let data = {
+      name,
+      email,
+      message,
+    };
+
+    fetch("api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("response received");
+      if (res.status === 200) {
+        console.log("success");
+        setSubmitted(true);
+        setName("");
+        setEmail("");
+        setMessage("");
+      }
+    });
+  };
+
   return (
     <footer className={styles.footer}>
-      <form className={styles.contact_form} id="contact">
+      <form
+        className={styles.contact_form}
+        id="contact"
+        onSubmit={handleSubmit}
+      >
         <h3>Want To Hire Me / Have Any Suggestions?</h3>
         <p>Simply Fill In The Form to Send Me An E-mail!</p>
         <div className={styles.input_group}>
           <label htmlFor="name">Your Name</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
           <label htmlFor="email">Your Email</label>
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
           <label htmlFor="message">Your Message</label>
-          <textarea name="message" rows={4} cols={50} />
+          <textarea
+            name="message"
+            rows={4}
+            cols={50}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+          />
         </div>
         <button>Submit</button>
       </form>
